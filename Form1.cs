b3f80939542852988542901b3b3d1a8600f11f37
Artemych907 <artemych907@gmail.com>
2023-10-06 12:53:20 +0600

    maximum method

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,42 @@ namespace Encrypt
     public partial class Form1 : Form
     {
         Dictionary<char, double> _inputDict = new Dictionary<char, double>();
+        Dictionary<char, double> _primaryDict = new Dictionary<char, double>
+        {
+            { 'о', 10.98},
+            { 'е' , 8.45 },
+            { 'а' , 8.01 },
+            { 'и' , 7.37 },
+            { 'н' , 6.70 },
+            { 'т' , 6.32 },
+            { 'с' , 5.47 },
+            { 'р' , 4.75},
+            { 'в' , 4.54 },
+            { 'л' ,  4.34},
+            { 'к' ,  3.49},
+            { 'м' ,  3.21},
+            { 'д' ,  2.98},
+            { 'п' ,  2.81},
+            { 'у' ,  2.62},
+            { 'я' ,  2.01},
+            { 'ы' ,  1.90},
+            { 'ь' ,  1.74},
+            { 'г' ,  1.69},
+            { 'з' ,  1.64},
+            { 'б' ,  1.59},
+            { 'ч' ,  1.45},
+            { 'й' ,  1.21},
+            { 'х' ,  0.97},
+            { 'ж' ,  0.94},
+            { 'ш' ,  0.72},
+            { 'ю' ,  0.64},
+            { 'ц' ,  0.49},
+            { 'щ' ,  0.36},
+            { 'э' ,  0.33},
+            { 'ф' ,  0.27},
+            { 'ъ' ,  0.04},
+            { 'ё' ,  0.02}
+        };
 
         public Form1()
         {
@@ -31,10 +67,26 @@ namespace Encrypt
         }
         private void button2_Click(object sender, EventArgs e)
         {
+            inputTextBox.Text = outputTextBox.Text;
             if (radioCaesar.Checked)
             {
-                outputTextBox.Text = new Caesar((int)caesarKey.Value).Decrypt(inputTextBox.Text = outputTextBox.Text);
-                MessageBox.Show($"Ключ шифрования : {caesarKey.Value}", "Success", MessageBoxButtons.OK);
+                Dictionary<int, double> sumArray = new Dictionary<int, double>();
+
+                for (int i = 1; i < _primaryDict.Keys.Count; i++)
+                {
+                    var decryptText = new Caesar(-i).Encrypt(inputTextBox.Text);
+                    double value;
+                    sumArray[i] = decryptText.ToLower().Sum(ch => _primaryDict.TryGetValue(ch, out value) ? value : 0);
+                }
+
+                var caesarKey = sumArray.OrderByDescending(x => x.Value).First().Key;
+
+                MessageBox.Show($"Ключ шифрования : {caesarKey}", "Success", MessageBoxButtons.OK);
+
+                outputTextBox.Text = new Caesar(caesarKey * (-1)).Encrypt(inputTextBox.Text);
+                //outputTextBox.Text = new Caesar((int)caesarKey.Value).Decrypt(inputTextBox.Text = outputTextBox.Text);
+
+                //MessageBox.Show($"Ключ шифрования : {caesarKey}", "Success", MessageBoxButtons.OK);
             }
             if (radioTritemius.Checked)
             {
@@ -42,7 +94,7 @@ namespace Encrypt
             }
             if (radioMonoCode.Checked)
             {
-                outputTextBox.Text = new MonoEncoder().Decrypt(inputTextBox.Text = outputTextBox.Text);
+                outputTextBox.Text = new MonoEncoder().Decrypt(inputTextBox.Text);
             }
         }
         private void btnOpenFile_Click(object sender, EventArgs e)
